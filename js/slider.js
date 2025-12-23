5f5bc9a3f91e02bd357725f084f29c5183c4fa70
hescalag <hernan.escalantegarcia@cencosud.cl>
2025-12-23 12:07:27 -0300

    inicios

---
+++ js/slider.js
@@ -0,0 +1,58 @@
+const slides = document.querySelectorAll(".slide");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const dotsContainer = document.querySelector(".dots");
+
+let current = 0;
+let interval;
+
+// Crear dots
+slides.forEach((_, i) => {
+  const dot = document.createElement("span");
+  if (i === 0) dot.classList.add("active");
+  dot.addEventListener("click", () => goToSlide(i));
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".dots span");
+
+function showSlide(index) {
+  slides.forEach(s => s.classList.remove("active"));
+  dots.forEach(d => d.classList.remove("active"));
+
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+  current = index;
+}
+
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+
+function prevSlide() {
+  current = (current - 1 + slides.length) % slides.length;
+  showSlide(current);
+}
+
+function goToSlide(index) {
+  showSlide(index);
+  resetInterval();
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(nextSlide, 6000);
+}
+
+next.addEventListener("click", () => {
+  nextSlide();
+  resetInterval();
+});
+
+prev.addEventListener("click", () => {
+  prevSlide();
+  resetInterval();
+});
+
+interval = setInterval(nextSlide, 6000);

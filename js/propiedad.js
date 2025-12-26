97e7e34bb9e8c28f3740b681b21232f3965eff3d
hescalag <hernan.escalantegarcia@cencosud.cl>
2025-12-26 10:46:43 -0300

    inicio

---
+++ js/propiedad.js
@@ -0,0 +1,31 @@
+fetch('json/propiedades.json')
+  .then(res => res.json())
+  .then(data => {
+    const prop = data[0]; // o buscar por ID
+
+    document.getElementById('titulo').innerText =
+      `${prop.tipo} en ${prop.comuna}`;
+
+    document.getElementById('precio').innerText =
+      `UF ${prop.precio_uf}`;
+
+    document.getElementById('datos-generales').innerHTML = `
+      <p><strong>Código:</strong> ${prop.id}</p>
+      <p><strong>Ciudad:</strong> ${prop.ciudad}</p>
+      <p><strong>Dirección:</strong> ${prop.direccion}</p>
+      <p><strong>Orientación:</strong> ${prop.orientacion}</p>
+    `;
+
+    document.getElementById('datos-tecnicos').innerHTML = `
+      <p><strong>Dormitorios:</strong> ${prop.dormitorios}</p>
+      <p><strong>Baños:</strong> ${prop.banos}</p>
+      <p><strong>Estacionamientos:</strong> ${prop.estacionamientos}</p>
+      <p><strong>Sup. total:</strong> ${prop.superficie_total} m²</p>
+      <p><strong>M² útiles:</strong> ${prop.superficie_util} m²</p>
+    `;
+
+    const galeria = document.getElementById('galeria');
+    prop.imagenes.forEach(img => {
+      galeria.innerHTML += `<img src="${img}" />`;
+    });
+  });

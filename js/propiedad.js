81ae880358716d98814fc545954e05f9f0cd84c6
hescalag <hernan.escalantegarcia@cencosud.cl>
2025-12-31 12:22:52 -0300

    inicios

--- js/propiedad.js
+++ js/propiedad.js
@@ -29,7 +29,7 @@ fetch('json/propiedad.json')
       `${prop.tipo} en ${prop.ciudad} - ${prop.comuna}`;
     
     document.getElementById('valor').innerText =
-      `Precio: ${prop.precio_uf}`;
+      `Precio: ${prop.precio_uf} UF`;
 
     document.getElementById('datos').innerHTML = `
         <li><strong>Código:</strong> ${prop.id}</li>
